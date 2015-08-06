5ce89257d0c31cd635a29440a4b48cd074d4bcf0
Durgesh Kumar Patidar <durgesh.patidar@gspann.om>
2015-08-07 02:35:19 +0530

    change local path

---
+++ java_workspace/src/main/java/com/owcs/App.java
@@ -0,0 +1,38 @@
+package com.owcs;
+
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	
+//        System.out.println( "###############Hello World!##############" );
+//        final File folder = new File("G:/Reliance/JSK/Sites/11.1.1.8.0/csdt/csdt-client");
+//        listFilesForFolder(folder);
+    }
+    
+    public static void listFilesForFolder(final File folder) {
+//        for (final File fileEntry : folder.listFiles()) {
+//            if (fileEntry.isDirectory()) {
+//                listFilesForFolder(fileEntry);
+//            } else {
+//            	
+//            	Matcher matcher = Pattern.compile("([a-zA-Z]+)(\\d+)").matcher(fileEntry.getName());
+//            	if (matcher.matches()) {
+//            	    System.out.println(matcher.group(1) + " , " + matcher.group(2));
+//            	}
+//                System.out.println(fileEntry.getAbsoluteFile());
+//                
+//                
+//            }
+//        }
+    }
+}
